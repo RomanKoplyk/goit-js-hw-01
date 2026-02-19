e932db8e47e5e128a51571372ae14166dca716b4
Roman Koplyk <romasve94@gmail.com>
2026-02-19 20:47:36 +0100

    add function

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    console.log(totalPrice)
     return totalPrice
 }
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,13 @@
 function getElementWidth(content, padding, border) {
+    const contNum = Number.parseFloat(content);
+    const paddNum = Number.parseFloat(padding);
+    const bordNum = Number.parseFloat(border);
 
+    const boxSizing = contNum + (paddNum * 2) + (bordNum * 2);
+    return boxSizing;
 }
+  
+getElementWidth()
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
